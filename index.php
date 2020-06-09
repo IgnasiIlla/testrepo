e78ac11a502860d278cf0bff88d1dac92051334b
IgnasiIlla <47320598+IgnasiIlla@users.noreply.github.com>
2020-06-09 12:44:26 +0200

    Add files via upload

---
+++ index.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: blackwood
+ * Date: 9/06/20
+ * Time: 12:34
+ */
+
+//I had no more time, so I decided to send this brief comments about what should be done
+
+// Get data from our json textfile
+$myData = json_decode("./step4-payload.json");
+
+// prepare data as an array (easier to manage than an stdclass for output purposes)
+
+//print data using a loop, using html and css
+
+
+?>
